3c4c34e2002a1843afaf989f6b09f869ff712b37
Alex Miles Younger <alex@alxpck.com>
2016-06-28 07:41:16 -0400

    Create the database (manually via MySQL Workbench) and tables (programatically)

--- diary.py
+++ diary.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import datetime
 from peewee import *
 
 # MySQL connection
@@ -5,10 +7,20 @@ db = MySQLDatabase("diary", user="root", passwd="pass123")
 
 class Entry(Model):
 	# content
+	# We're not using a VARCHAR because varchar's require a max length, and we don't want to explicitly define a limit. 
+	content = TextField()
 	# timestamp
+	# when it creates a default it will call the now() function, but because we left off the parenthesis it only runs the function when the default is created.
+	timestamp = DateTimeField(default=datetime.datetime.now)
 
 	class Meta:
 		database = db
+
+def initialize():
+	"""Create the database and the table if they don't exist."""
+	db.connect()
+	# different than the tutorial, but I like to make the tables plural of the things they will contain
+	db.create_tables([Entries], safe=True)
 
 def menu_loop():
 	"""Show the menu"""
@@ -26,4 +38,5 @@ def delete_entry(entry):
 	"""Delete an entry."""
 
 if __name__ == '__main__':
+	initialize()
 	menu_loop()
